00b63c3e28d06b181e0d972cdd089b7ee3459fe9
hoanggyen231 <147621826+galina723@users.noreply.github.com>
2024-04-23 14:27:55 +0700

    Add files via upload

---
+++ Labcse305/RentalProperty/src/Userr.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+import java.io.*;
+import java.util.*;
+
+public class Userr {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Userr(String Id, String password, String email, String UserType) {
+        this.Id = Id;
+        this.password = password;
+        this.email = email;
+        this.UserType = UserType;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return UserType;
+    }
+
+    public void setUserType(String UserType) {
+        this.UserType = UserType;
+    }
+    
+    String Id;
+    String password;
+    String email;
+    String UserType;
+    
+}
